b4fb7ef832433b818b767b119705841d3621b45c
Ciarango10 <carlosarango3101@gmail.com>
2024-04-14 16:49:46 -0500

    Crud de Estudiante

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/home', [HomeController::class, 'index'])->name('home.index');
 
+#Estudiantes
 Route::get('/students', [StudentsController::class, 'index'])->name('students.index');
+Route::get('/students/create', [StudentsController::class, 'create'])->name('students.create');
+Route::get('/students/edit/{id}', [StudentsController::class, 'edit'])->name('students.edit');
+
+Route::post('/students/store', [StudentsController::class, 'store'])->name('students.store');
+Route::put('/students/update', [StudentsController::class, 'update'])->name('students.update');
+Route::delete('/students/delete/{id}', [StudentsController::class, 'delete'])->name('students.delete');
